7501554d2f37b905bd786834c586b6184e584a2d
IAMolofficial <amolpatil147007@gmail.com>
2023-04-19 14:34:10 +0530

    changes in header and frontend file

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -10,7 +10,7 @@ const Frontend = () => {
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">HTML</h3>
-              <span className="skills__level">Basic</span>
+              <span className="skills__level">Advance</span>
             </div>
           </div>
           <div className="skills__data">
